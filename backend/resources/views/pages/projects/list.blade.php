71585d2271a933db4df9d1b41d47a9ac55ca8693
nagairy <nagairy@star10star.com>
2022-01-13 18:11:43 +0900

    Create projects page

---
+++ backend/resources/views/pages/projects/list.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <header>
+        <title>プロジェクト一覧</title>
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <script src="{{ asset('js/app.js') }}"></script>
+    </header>
+    <body>
+        <div>プロジェクトの一覧画面です。</div>
+    </body>
+</html>
