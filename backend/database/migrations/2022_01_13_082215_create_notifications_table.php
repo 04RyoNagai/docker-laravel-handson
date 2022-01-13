cb625c8c3fb2e73ab6e2bc80aa2bb5139b279583
nagairy <nagairy@star10star.com>
2022-01-13 17:39:11 +0900

    Create notifications table

---
+++ backend/database/migrations/2022_01_13_082215_create_notifications_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifications', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->text('message');
+            $table->string('target_type', 254);
+            $table->unsignedBigInteger('target_id');
+            $table->string('status', 254)->default('unread');
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifications');
+    }
+}
