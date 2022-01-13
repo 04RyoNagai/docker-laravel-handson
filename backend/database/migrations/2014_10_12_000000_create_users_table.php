a055c45b0c8ae9aff4883cf8f72b8ba620b13fa5
nagairy <nagairy@star10star.com>
2022-01-13 16:46:09 +0900

    Create users table

--- backend/database/migrations/2014_10_12_000000_create_users_table.php
+++ backend/database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,12 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->string('name', 254);
+            $table->string('login_id', 254)->unique();
+            $table->string('password_digest', 254);
+            $table->string('mail', 254)->unique();
+            $table->string('next_mail', 254)->unique()->nullable()->default(null);
+            $table->string('token', 254)->unique();
+            $table->string('reset_password_token', 254)->unique()->nullable();
+            $table->timestamp('reset_password_expire_time')->nullable();
+            $table->string('confirmation_token', 254)->unique()->nullable();
+            $table->timestamp('confirmation_expire_time')->nullable();
+            $table->timestamp('confirmed_at')->nullable();
+            $table->timestamp('deleted_at')->nullable();
+            $table->timestamp('created_at');
+            $table->timestamp('updated_at');
         });
     }
 
