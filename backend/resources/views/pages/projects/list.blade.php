19b178ee65624766a7deb84774b6cad2f7257039
nagairy <nagairy@star10star.com>
2022-01-14 13:55:49 +0900

    Create project list view

--- backend/resources/views/pages/projects/list.blade.php
+++ backend/resources/views/pages/projects/list.blade.php
@@ -1,11 +1,52 @@
 <!DOCTYPE html>
 <html lang="ja">
     <header>
-        <title>プロジェクト一覧</title>
+        <title>プロジェクト一覧画面</title>
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
         <script src="{{ asset('js/app.js') }}"></script>
+        <script src="{{ asset('js/projects.js')}}"></script>
     </header>
     <body>
-        <div>プロジェクトの一覧画面です。</div>
+        <h1>プロジェクト一覧</h1>
+        <input type="button" class="new-project-button" value="プロジェクト作成">
+        <div class="select-status">
+            <input type="radio" name="status" value="open">Open
+            <input type="radio" name="status" value="closed">Closed
+            <input type="radio" name="status">All
+        </div>
+        <div class="sort-container">
+            <select name="sort" size="1">
+                <option value="title">タイトル</option>
+                <option value="updated">最終更新時刻</option>
+            </select>
+            <div class="sort-switch">
+                <input type="checkbox" id="sort-btn" checked="">
+                    <label for="sort-btn">
+                        <span></span>
+                    </label>
+            </div>
+        </div>
+        <table class="table-bordered col-md-11 project-list">
+            @foreach ($projects as $project)
+                <tr>
+                    <td class="col-md-8">
+                        <label class="project-title">{{$project->title}}</label>
+                        <div>
+                            <label>{{$project->description}}</label>
+                        </div>
+                    </td>
+                    <td class="col-md-2">
+                        <div>
+                            <label>last updated at</label>
+                        </div>
+                        <label>{{$project->updated_at}}</label>
+                    </td>
+                    <td class="col-md-1">
+                        <label>{{$project->status}}</label>
+                    </td>
+                </tr>
+            @endforeach
     </body>
 </html>
