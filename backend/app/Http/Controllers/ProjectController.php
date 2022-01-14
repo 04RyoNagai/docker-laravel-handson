35e5b6cc8c168096b043da4730e59513b877099c
nagairy <nagairy@star10star.com>
2022-01-14 13:55:15 +0900

    Create Project model and update ProjectController

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
     public function index()
     {
-        return view('pages.projects.list');
+        $projects = Project::all();
+        return view('pages.projects.list', compact('projects'));
     }
 }
