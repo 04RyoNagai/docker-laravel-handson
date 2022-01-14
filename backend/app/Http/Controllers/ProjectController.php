6653231314b8e2374c14c4dc99769796fac62ed8
nagairy <nagairy@star10star.com>
2022-01-14 16:46:04 +0900

    Add route for new project page

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -12,4 +12,18 @@ class ProjectController extends Controller
         $projects = Project::all();
         return view('pages.projects.list', compact('projects'));
     }
+
+    public function new()
+    {
+        return view('pages.projects.new');
+    }
+
+    public function create(Request $request)
+    {
+        $project = new Project;
+        $project->title = $request->input('title');
+        $project->description = $request->input('description');
+        $project->save();
+        return redirect('/projects');
+    }
 }
